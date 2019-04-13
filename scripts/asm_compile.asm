ea1d8b58edc1344d6bc2fa27b5bb1056d8afaaa9
Ryan Newell <ryansnewell@gmail.com>
2019-04-12 20:39:08 -0500

    Added a python script that uses the MARS compiler and emulator to retreive program output. Also, added a test assmebly program.

---
+++ scripts/asm_compile.asm
@@ -0,0 +1,26 @@
+import subprocess
+
+def sim_asm(asm_file=None):
+
+    if asm_file:
+        path = asm_file
+    else:
+        print('Please provide an assembly file to run.')
+        path = input('>').strip()
+        print('Sim-ing input file: ' + path)
+    
+    mars_dump_file = open('temp/mars_dump.out', 'w+')
+    subprocess.call(
+            ['java','-jar','Mars_CPRE381_SIMD_v1.jar','nc',path],
+            stdout=mars_dump_file
+    )
+    mars_dump_file.close()
+
+if __name__ == '__main__':
+        try:
+            sim_asm()
+        except KeyboardInterrupt:
+            exit(1)
+        except Exception as e:
+            print('Exited with unexpected excetpion:\n')
+            print(e)
